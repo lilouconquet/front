eb551efa9f4018851bf91f81621c617da985ee32
Jerome Bellon <jerome.bellon@edu.escp.eu>
2021-09-02 11:18:45 +0200

    Mise en forme du streamlit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from typing import ValuesView
 import streamlit as st
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -10,14 +11,14 @@ image_header = Image.open('Author_style.png')
 st.header('''Projet Style d'un auteur''')
 st.image(image_header, width=800)
 
-st.markdown("""# Qui est l'auteur qui aurait pu écrire ce paragraphe ?
-## Découvrons-le grâce à notre modèle !
-# """)
+st.markdown(
+    f"<h1 style='text-align: center; color: black;'>Qui est l'auteur qui aurait pu écrire ce paragraphe ?</h1>",
+    unsafe_allow_html=True)
+st.markdown(
+    f"<h2 style='text-align: center; color: black;'>Découvrons-le grâce à notre modèle !</h2>",
+    unsafe_allow_html=True)
 
 text_input = st.text_input("""Copiez un paragraphe d'auteur ici""")
-
-length = st.write('Nombre de caractères dans le paragraphe:', len(text_input))
-
 
 prediction = st.button('Prédire')
 
@@ -28,6 +29,7 @@ if prediction == True:
         f'https://apiamd64-ywkwoqtfqq-ew.a.run.app/predict?paragraph={text_input}',
     ).json()
     auteurs = response['prediction']
+    auteurs = dict(sorted(auteurs.items(), key=lambda item: item[1]))
     keys = list(auteurs.keys())
     values = list(auteurs.values())
 
@@ -36,15 +38,20 @@ if prediction == True:
 
     root_path = os.path.dirname(__file__)
     path_folder = os.path.join(root_path, 'Auteurs_photos')
-    images = [image for image in os.listdir(path_folder)]
-    max = max(auteurs.values())
+
+    auteur_nom = max_auteur.split(" ")
+    auteur_nom = auteur_nom[1] + ' ' + auteur_nom[0]
+
+    st.markdown(
+        f"<h1 style='text-align: center; color: black;'>L'auteur le plus probable est {auteur_nom}</h1>",
+        unsafe_allow_html=True)
 
     col1, col2 = st.columns(2)
     with col1:
         st.text(' ')
         imag1 = Image.open(
             os.path.join(path_folder, ''.join([max_auteur, '.png'])))
-        st.image(imag1, width=400)
+        st.image(imag1, width=600)
 
     with col2:
         fig = plt.figure(figsize=(5, 5))
@@ -54,3 +61,6 @@ if prediction == True:
         plt.title("Prédiction du style de l'auteur")
         st.pyplot(fig)
 
+#st.header("""C'est Simone de Beauvoire""")
+#image = Image.open('Simone_de_Beauvoir.png')
+#st.image(image, caption='Simone de Beauvoir', width=200)
